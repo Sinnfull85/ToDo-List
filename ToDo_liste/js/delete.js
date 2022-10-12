13b29228bb2cd07ea2f966e08c21180b50bc5ad8
Marwa Laatil <elatilmarwa@gmail.com>
2022-10-12 18:27:10 +0200

    delet erste version

---
+++ ToDo_liste/js/delete.js
@@ -0,0 +1,11 @@
+document.addEventListener("click", function (e) {
+    // Delete Task
+    if (e.target.className == "mv-delete") {
+        console.log("this Is our Element");
+
+        const parent = e.target.parentNode.parentNode;
+        console.log(parent);
+
+        parent.remove();
+    }
+});
