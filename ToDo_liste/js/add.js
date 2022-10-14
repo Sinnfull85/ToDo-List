662de59a7b69951d0c1c77e3c42d594077416bc6
Marwa Laatil <elatilmarwa@gmail.com>
2022-10-14 16:33:30 +0200

    weiter

--- ToDo_liste/js/add.js
+++ ToDo_liste/js/add.js
@@ -1,8 +1,6 @@
 const APP = document.getElementById("App");
 const AddButton = document.getElementById("AddButton");
 const input = document.getElementById("addInput");
-// const detailsInpt = document.getElementById("details");
-// const datetimeInput = document.getElementById("datetime");
 
 let inputList = [];
 
@@ -12,25 +10,8 @@ function handleInput(event) {
     // input.value = "";
     console.log(inputList);
 }
-let inputList2 = [];
 
-function handleInput2(event) {
-    const newInput = event.target.value;
-    inputList2.push(newInput);
-    // input.value = "";
-    console.log(inputList2);
-}
-let inputList3 = [];
-function handleInput3(event) {
-    const newInput = event.target.value;
-    inputList3.push(newInput);
-    // input.value = "";
-    console.log(inputList3);
-}
 input.addEventListener("change", handleInput);
-
-// detailsInpt.addEventListener("change", handleInput2);
-// datetimeInput.addEventListener("change", handleInput3);
 
 AddButton.addEventListener("click", handleButtonClick);
 
@@ -42,11 +23,6 @@ function handleButtonClick() {
     const liAlsInput = document.createElement("input");
     liAlsInput.setAttribute("id", "li-als-input");
 
-    // const details = document.createElement("details");
-    // const summary = document.createElement("summary");
-
-    // const datetime = document.createElement("div");
-
     const editBtn = document.createElement("button");
     const deleteBtn = document.createElement("button");
 
@@ -56,26 +32,18 @@ function handleButtonClick() {
     editBtn.setAttribute("id", "mv-edit-2");
     deleteBtn.setAttribute("id", "mv-delete-2");
 
-    // datetime.setAttribute("id", "datetime");
-
     liAlsInput.value = inputList[inputList.length - 1];
-    console.log("GHfdj", inputList[inputList.length - 1]);
-    // summary.innerText = inputList2[inputList2.length - 1];
-    // datetime.innerHTML = inputList3[inputList2.length - 1];
+
     console.log("button clicked");
 
     input.value = "";
 
-    // detailsInpt.value = "";
-    // datetimeInput.value = "";
-
     APP.appendChild(listContainer);
     listContainer.appendChild(list);
     list.appendChild(li);
-    // details.appendChild(summary);
+
     li.appendChild(liAlsInput);
-    // li.appendChild(details);
-    // li.appendChild(datetime);
+
     listContainer.appendChild(editBtn);
     listContainer.appendChild(deleteBtn);
 }
