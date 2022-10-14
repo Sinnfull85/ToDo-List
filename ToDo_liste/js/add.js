bf5edfd1ed97229f30756b0415d5207f5950e765
Marwa Laatil <elatilmarwa@gmail.com>
2022-10-14 17:48:16 +0200

    weiter mit add.js und css

--- ToDo_liste/js/add.js
+++ ToDo_liste/js/add.js
@@ -4,8 +4,23 @@ const input = document.getElementById("addInput");
 
 let inputList = [];
 
+// beispiel
+// let inputList_v1 = ["kaufen", "Kochen"];
+// let inputList_v2 = [
+//     { aufgabe: "kaufen", zeit: "" },
+//     { aufgabe: "Kochen" },
+//     { aufgabe: "spielen" },
+// ];
+
+// let inputList_v3 = [
+//     { aufgabe: "kaufen", mehrdetais: "Milch, käse", datum: "heute" },
+//     { aufgabe: "Kochen", mehrdetais: "Milch, käse", datum: "heute" },
+//     { aufgabe: "spielen", mehrdetais: "", datum: "heute Abend" },
+// ];
+
 function handleInput(event) {
     const newInput = event.target.value;
+
     inputList.push(newInput);
     // input.value = "";
     console.log(inputList);
@@ -17,22 +32,26 @@ AddButton.addEventListener("click", handleButtonClick);
 
 function handleButtonClick() {
     const listContainer = document.createElement("div");
-    listContainer.setAttribute("id", "divToDo");
     const list = document.createElement("ul");
     const li = document.createElement("li");
     const liAlsInput = document.createElement("input");
-    liAlsInput.setAttribute("id", "li-als-input");
-
+    const divButton = document.createElement("div");
     const editBtn = document.createElement("button");
-    editBtn.setAttribute("class", "mv-edit");
     const deleteBtn = document.createElement("button");
-    deleteBtn.setAttribute("class", "mv-delete");
+
+    listContainer.setAttribute("class", "divToDo");
+    // listContainer.setAttribute("id", "divToDo");
+    liAlsInput.setAttribute("class", "li-als-input");
+    // liAlsInput.setAttribute("id", "li-als-input");
+    divButton.setAttribute("class", "divButton");
+    editBtn.setAttribute("class", "edit");
+    deleteBtn.setAttribute("class", "delete");
 
     editBtn.innerHTML = "Bearbeiten";
     deleteBtn.innerHTML = "Löschen";
 
-    editBtn.setAttribute("id", "mv-edit-2");
-    deleteBtn.setAttribute("id", "mv-delete-2");
+    // editBtn.setAttribute("id", "mv-edit-2");
+    // deleteBtn.setAttribute("id", "mv-delete-2");
 
     liAlsInput.value = inputList[inputList.length - 1];
 
@@ -40,14 +59,13 @@ function handleButtonClick() {
 
     input.value = "";
 
-    APP.appendChild(listContainer);
-    listContainer.appendChild(list);
-    list.appendChild(li);
-
     li.appendChild(liAlsInput);
-
-    listContainer.appendChild(editBtn);
-    listContainer.appendChild(deleteBtn);
+    list.appendChild(li);
+    listContainer.appendChild(list);
+    divButton.appendChild(editBtn);
+    divButton.appendChild(deleteBtn);
+    listContainer.appendChild(divButton);
+    APP.appendChild(listContainer);
 }
 
 // ToDO Array
